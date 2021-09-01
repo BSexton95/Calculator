2d8ec1c5dc47ae08dd106e4bb4da35e482a47d01
Brittnie <brittniesexton95@gmail.com>
2021-09-01 12:41:00 -0500

    Added comments

--- Calculator/Game.cs
+++ Calculator/Game.cs
@@ -6,32 +6,68 @@ namespace Calculator
 {
     class Game
     {
+        //Calculators answer
         float answer;
 
+        /// <summary>
+        /// Adds two values
+        /// </summary>
+        /// <param name="num1">First value</param>
+        /// <param name="num2">Second value</param>
+        /// <returns></returns>
         float Add(float num1, float num2)
         {
             return num1 + num2;
         }
 
+        /// <summary>
+        /// Subtracts two values
+        /// </summary>
+        /// <param name="num1">First value</param>
+        /// <param name="num2">Second value</param>
+        /// <returns></returns>
         float Subtraction(float num1, float num2)
         {
             return num1 - num2;
         }
 
+        /// <summary>
+        /// Divides two values
+        /// </summary>
+        /// <param name="num1">First value</param>
+        /// <param name="num2">Second value</param>
+        /// <returns></returns>
         float Division(float num1, float num2)
         {
             return num1 / num2;
         }
 
+        /// <summary>
+        /// Multiplys two values
+        /// </summary>
+        /// <param name="num1">First value</param>
+        /// <param name="num2">Second value</param>
+        /// <returns></returns>
         float Multiplication(float num1, float num2)
         {
             return num1 * num2;
         }
 
+        /// <summary>
+        /// Function takes two input values from user, converts them to floats, and preforms operation 
+        /// user has selected to use.
+        /// </summary>
+        /// <param name="description">Tells user to choose operatio they would like to use</param>
+        /// <param name="add"></param>
+        /// <param name="sub"></param>
+        /// <param name="div"></param>
+        /// <param name="mult"></param>
+        /// <returns></returns>
         float Calculate(string description, string add, string sub, string div, string mult)
         {
             string input = "";
 
+            //Displays menu user
             Console.WriteLine(description);
             Console.WriteLine(add);
             Console.WriteLine(sub);
@@ -40,8 +76,9 @@ namespace Calculator
 
             input = Console.ReadLine();
 
-            
+            //user input first value
             string value1 = "";
+            //user input second value
             string value2 = "";
             float num1;
             float num2;
@@ -50,41 +87,47 @@ namespace Calculator
             value1 = Console.ReadLine();
             value2 = Console.ReadLine();
 
+            //Converts the first value from a string to a float
             if (!float.TryParse(value1, out num1))
             {
                 Console.WriteLine("Invalid Input!");
             }
 
+            //Converts the second value from a string to a float
             if (!float.TryParse(value2, out num2))
             {
                 Console.WriteLine("Invaild Input!");
             }
 
-
+            //If user wants to add two numbers
             if (input == "1")
             {
                 answer = Add(num1, num2);
                 Console.WriteLine(answer);
                 
             }
+            //If user wants to subtract two numbers
             else if (input == "2")
             {
                 answer = Subtraction(num1, num2);
                 Console.WriteLine(answer);
                 
             }
+            //If user wants to divide two numbers
             else if (input == "3")
             {
                 answer = Division(num1, num2);
                 Console.WriteLine(answer);
                 
             }
+            //If user wants to multiply two numbers
             else if (input == "4")
             {
                 answer = Multiplication(num1, num2);
                 Console.WriteLine(answer);
                 
             }
+            //User enter and invalid input
             else
             {
                 Console.WriteLine("Invalid Input");
@@ -100,33 +143,11 @@ namespace Calculator
 
             int inputRecieved = 0;
 
+            //While loop, loops user through choosing an operation after ever calculation
             while (!(inputRecieved == 1 || inputRecieved == 2 || inputRecieved == 3 || inputRecieved == 4))
             {
-                Calculate("Which operation would you like to preform?", "\n Enter 1 for Addition", "" +
+                Calculate("Which operation would you like to preform?", "\n Enter 1 for Addition",
                     "\n Enter 2 for Subtraction", "\n Enter 3 for Division", "\n Enter 4 for Multiplication");
-                /*
-                bool calculate = false;
-                while(!calculate)
-                {
-                    Console.WriteLine("Would you like to calculate more numbers?" + "\n yes" + "\n no");
-                    Console.ReadLine();
-
-                    string input = Console.ReadLine();
-
-                    if( input == "yes")
-                    {
-                        Calculate("Which operation would you like to preform?", "\n Enter 1 for Addition", "" +
-                         "\n Enter 2 for Subtraction", "\n Enter 3 for Division", "\n Enter 4 for Multiplication");
-                    }
-                    else if(input == "no")
-                    {
-                        
-                    }
-                    else
-                    {
-                        Console.WriteLine("Invalid Input!");
-                    }
-                }*/
             }
         }
     }
